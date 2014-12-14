8af57f480ca93b93ae1c9233ccf2ffae98584b81
Hamza HIKMAT <hamzahik@gmail.com>
2014-12-14 11:27:40 +0000

    add parser and utils modules

---
+++ ELFParser/index.js
@@ -0,0 +1,83 @@
+﻿var journal			= null;
+var journalStr		= null;
+var journalLines	= null;
+var journalLinesNbr	= null;
+var output			= {meta:{},lines:[]};
+var fields			= null;
+var fieldsLength	= null;
+var extractors		= require('ELFParser/extractors');
+
+exports.parse = function( path ){
+	
+	journal			= File( path );
+	//TODO: optimize by using textstream
+	journalStr		= journal.toString();
+	journalLines	= journalStr.split('\n');
+	journalLinesNbr	= journalLines.length;
+
+	for ( var  i = 0 ; i < journalLinesNbr ; ++i ) {
+	
+		var line		= journalLines[ i ];
+		
+		var firstChar	= line.charAt( 0 );
+		
+		if ( firstChar == '#' ) {
+		
+			readMeta( output , journalLines[ i ] );
+			
+			continue;
+			
+		}
+		
+		if ( line.match(/^[ \t]*$/) ) {
+		
+			continue;
+			
+		}
+		
+		output.lines[ i ] = readLine( journalLines[ i ] );
+
+
+	};
+	
+	return output;
+
+}
+
+function readMeta( output , line ){
+
+		
+	var parsedLine	= line.match(/^#([^:]+):[\t ]*(.*)/);
+	
+	output.meta[ parsedLine[ 1 ] ]	= parsedLine[ 2 ];
+	
+	if ( parsedLine[ 1 ] == "Fields" ) {
+	
+		fields 			= parsedLine[ 2 ].split(' ');
+		fieldsLength	= fields.length;
+	
+	}
+	
+	return true;
+
+}
+
+function readLine( line ){
+
+	var parsedLine	= {};
+	
+	for ( var i = 0 ; i < fieldsLength ; ++i ) {
+		
+		var field	= fields[ i ];
+		
+		var match	= line.match( extractors[ field ] );
+		
+		parsedLine[ field ]	= match[ 1 ];
+		
+		line	= line.trim().substring( match[ 0 ].length );
+		
+	}
+	
+	return parsedLine;
+
+}
---
+++ logUtils/index.js
@@ -0,0 +1,155 @@
+﻿exports.getLinesByFieldValue	= function( lines , field , value ){
+	
+	var linesNbre	= lines.length;
+	var subLines	= [];
+	
+	for ( var i = 0 ; i < linesNbre ; ++i ) {
+	
+		if ( lines[ i ] && lines[ i ][ field ] == value ) {
+		
+			subLines.push( lines[ i ] );
+		
+		}
+	
+	}
+	
+	return subLines;
+
+};
+
+exports.getLinesByMatcher	= function( lines , matcher ){
+	
+	var linesNbre	= lines.length;
+	var subLines	= [];
+	
+	for ( var i = 0 ; i < linesNbre ; ++i ) {
+	
+		if ( lines[ i ] && matcher( lines[ i ] ) ) {
+		
+			subLines.push( lines[ i ] );
+		
+		}
+	
+	}
+	
+	return subLines;
+
+};
+
+exports.countLinesByMatcher	= function( lines , matcher ){
+	
+	var linesNbre	= lines.length;
+	var subLines	= [];
+	
+	for ( var i = 0 ; i < linesNbre ; ++i ) {
+	
+		if ( lines[ i ] && matcher( lines[ i ] ) ) {
+		
+			subLines.push( i );
+		
+		}
+	
+	}
+	
+	return {
+		
+		count : subLines.length,
+	
+		lines : subLines
+		
+	};
+
+};
+
+exports.countLinesByFieldValue	= function( lines , field , value ){
+	
+	var linesNbre	= lines.length;
+	var subLines	= [];
+	
+	for ( var i = 0 ; i < linesNbre ; ++i ) {
+	
+		if ( lines[ i ] && lines[ i ][ field ] == value ) {
+		
+			subLines.push( i );
+		
+		}
+	
+	}
+	
+	return {
+		
+		count : subLines.length,
+	
+		lines : subLines
+		
+	};
+
+};
+
+exports.postProcess	= function( lines , processor ){
+	
+	var linesNbre	= lines.length;
+	
+	for ( var i = 0 ; i < linesNbre ; ++i ) {
+	
+		if ( lines[ i ] ) {
+		
+			processor( lines[ i ] );
+		
+		}
+	
+	}
+
+};
+
+exports.average	= function( lines , field ){
+	
+	var linesNbre	= lines.length;
+	var min			= Infinity;
+	var max			= 0;
+	var sum			= 0;
+	var count		= 0;
+	var average		= 0;
+	
+	for ( var i = 0 ; i < linesNbre ; ++i ) {
+	
+		var line	= lines[ i ];
+		
+		if ( line ) {
+			
+			var value	= line[ field ];
+		
+			if ( value < min ) {
+			
+				min	= value;
+			
+			}
+			
+			if ( value > max ) {
+			
+				max	= value;
+			
+			}
+			
+			sum += value;
+			count++;
+		
+		}
+	
+	}
+	
+	average	= ( count )? sum/count : 0 ;
+	
+	return {
+	
+		count	: count,
+		
+		average	: average,
+		
+		min		: min,
+		
+		max		: max
+	
+	};
+
+};
